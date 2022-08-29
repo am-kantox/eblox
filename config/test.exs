79a9b25eb5487e56a9fdd4bd673763ddd8bd6509
Aleksei Matiushkin <aleksei.matiushkin@kantox.com>
2022-08-29 06:23:37 +0200

    Fixed PostgreSQL creds in CI

--- config/test.exs
+++ config/test.exs
@@ -6,8 +6,9 @@ import Config
 # to provide built-in test partitioning in CI environment.
 # Run `mix help test` for more information.
 config :eblox, Eblox.Repo,
+  username: "postgres",
+  password: "postgres"
   database: "eblox_test#{System.get_env("MIX_TEST_PARTITION")}",
-  hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox,
   pool_size: 10
 
